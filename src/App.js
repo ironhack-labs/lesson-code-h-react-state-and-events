1fbcc5a0400619c9de866a97433e12bccf778ab6
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-06 23:47:00 +0200

    Example 6

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import { useState } from "react";
 function App() {
   const [theme, setTheme] = useState("light");
 
+  const toggleTheme = (e) => {
+    setTheme(e.target.value);
+  };
+
   return (
     <div className={"App " + theme}>
       <h1>React - state and events</h1>
       <Counter />
 
-      <select onChange={(e) => setTheme(e.target.value)}>
+      <select onChange={toggleTheme}>
         <option value="light"> Light </option>
         <option value="dark"> Dark </option>
       </select>
