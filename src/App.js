d92ec9bd1bb191f3195323bae3e95e06c053c1e3
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-06 14:49:26 +0200

    Finished example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+import WeatherForecast from "./components/WeatherForecast";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  const [unit, setUnit] = useState("C");
+
+  const toggleTheme = (event) => {
+    const value = event.target.value; // value -->  'light' || 'dark'
+    setTheme(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App " + theme}>
+      <h1>React - state and events</h1>
+
+      <Counter />
+
+      <select onChange={toggleTheme}>
+        <option value="light"> Light </option>
+        <option value="dark"> Dark </option>
+      </select>
+      <br />
+
+      <button onClick={() => setUnit("C")}> °C </button>
+      <button onClick={() => setUnit("F")}> °F </button>
+
+      <WeatherForecast tempUnit={unit} theme={theme} />
+      {/* 
+      <div>
+        <WeatherWidget city="Miami" icon="🟡" unit={unit} celsius={29} />
+        <WeatherWidget city="Mexico City" icon="⛈️" unit={unit} celsius={17} />
+        <WeatherWidget city="Berlin" icon="🌥️" unit={unit} celsius={20} />
+        <WeatherWidget city="Barcelona" icon="🌤️" unit={unit} celsius={28} />
+      </div> 
+      */}
     </div>
   );
 }
