fe2bd1e87914387ca0dfaf333e9eb00c3430f5e5
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-06 14:54:44 +0200

    Example 2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 
 function Counter() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
   return (
     <div className="Counter">
       <h2>Counter</h2>
