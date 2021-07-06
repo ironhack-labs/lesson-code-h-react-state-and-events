5deddf9a19d486bc6d05a1a59ebd085d464c09eb
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-06 15:55:59 +0200

    Example 4

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 
 function Counter() {
-  // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
 
   return (
@@ -10,8 +9,8 @@ function Counter() {
 
       <p>You clicked {count} times</p>
 
-      <button> - </button>
-      <button> + </button>
+      <button onClick={() => setCount(count - 1)}> - </button>
+      <button onClick={() => setCount(count + 1)}> + </button>
     </div>
   );
 }
