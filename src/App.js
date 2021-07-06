8bdbc112e720e94c90858bd5acfbf7bcced3a300
ross-u <cirkovic.uros.srb@gmail.com>
2021-07-06 23:27:00 +0200

    Example 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Counter from "./components/Counter";
+import { useState } from "react";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
   return (
-    <div className="App ">
+    <div className={"App " + theme}>
       <h1>React - state and events</h1>
       <Counter />
+
+      <select onChange={(e) => setTheme(e.target.value)}>
+        <option value="light"> Light </option>
+        <option value="dark"> Dark </option>
+      </select>
     </div>
   );
 }
